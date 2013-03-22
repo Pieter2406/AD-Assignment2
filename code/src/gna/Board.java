529b6333eeb6e84246c10fa33f4ef3df809c85ec
Pieter2406 <pieterverlinden2406@hotmail.com>
2013-03-22 18:22:49 +0100

    Initial project version of the second AnD assignment

---
+++ code/src/gna/Board.java
@@ -0,0 +1,40 @@
+package gna;
+
+public class Board
+{
+	// construct a board from an N-by-N array of tiles
+	public Board( int[][] tiles )
+	{
+	}
+	
+	// return number of blocks out of place
+	public int hamming()
+	{
+		return 0;
+	}
+	
+	// return sum of Manhattan distances between blocks and goal
+	public int manhattan()
+	{
+		return 0;
+	}
+	
+	// does this board position equal y
+	public boolean equals(Object y)
+	{
+		return false;
+	}
+	
+	// return an Iterable of all neighboring board positions
+	public Iterable<Board> neighbors()
+	{
+		return null;
+	}
+	
+	// return a string representation of the board
+	public String toString()
+	{
+		return "<empty>";
+	}
+}
+
